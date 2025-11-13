5cf5b7031adcd6f0615ccb44ef05fdf013bde3c9
amberhandal <ambahjh@gmail.com>
2025-11-13 14:11:35 -0600

    implement flip node

--- diff_drive/setup.py
+++ diff_drive/setup.py
@@ -1,15 +1,28 @@
-from setuptools import find_packages, setup
 from pathlib import Path
+
+from setuptools import find_packages, setup
+
 
 def recursive_files(prefix, path):
     """
-    Recurse over path returning a list of tuples suitable for use with setuptools data_files.
-    :param prefix: prefix path to prepend to the path
-    :param path: Path to directory to recurse. Path should not have a trailing '/'
-    :return: List of tuples. First element of each tuple is destination path, second element is a list of files to copy to that path
+    Recurse over path returning a list of tuples.
+
+    :param prefix: prefix path to prepend to the path.
+    :param path: Path to directory to recurse.
+                 Path should not have a trailing '/'.
+    :return: List of tuples.
+             First element of each tuple is destination path.
+             Second element is a list of files to copy to that path.
+
     """
-    return [(str(Path(prefix)/subdir),
-             [str(file) for file in subdir.glob('*') if not file.is_dir()] ) for subdir in Path(path).glob('**')]
+    return [
+        (
+            str(Path(prefix) / subdir),
+            [str(file) for file in subdir.glob('*') if not file.is_dir()],
+        )
+        for subdir in Path(path).glob('**')
+    ]
+
 
 package_name = 'diff_drive'
 
@@ -18,15 +31,14 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/hook', ['env-hooks/diff_drive.dsv']),
-        *recursive_files('share/' + package_name, "models"),
-        *recursive_files('share/' + package_name, "worlds"),
-        *recursive_files('share/' + package_name, "launch"),
-        *recursive_files('share/' + package_name, "urdf"),
-        *recursive_files('share/' + package_name, "config"),
+        *recursive_files('share/' + package_name, 'models'),
+        *recursive_files('share/' + package_name, 'worlds'),
+        *recursive_files('share/' + package_name, 'launch'),
+        *recursive_files('share/' + package_name, 'urdf'),
+        *recursive_files('share/' + package_name, 'config'),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -41,6 +53,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'flip = diff_drive.flip:main',
         ],
     },
 )
